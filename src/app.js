e85f2f738d4ed4b0435d1f61fc93baed8a8cad63
shahtag <shashwatsatna@gmail.com>
2022-07-08 00:33:55 +0530

    ✨ search input

--- src/app.js
+++ src/app.js
@@ -7,23 +7,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/search", async (req, res) => {
-  const page = parseInt(req.body.page);
+
+
+app.get("/search/:page", async (req, res) => {
+  const page = parseInt(req.params.page);
+  try {
+    const result = await SearchItem.paginate({}, { page, limit: 5 });
+    res.json(result);
+  } catch (e) {}
+});
+
+app.get("/search/:search/:page", async (req, res) => {
+  const page = parseInt(req.params.page);
   try {
     const result = await SearchItem.paginate(
       {
         $or: [
-          { name: { $regex: "^" + req.body.search, $options: "i" } },
-          { symbol: { $regex: "^" + req.body.search, $options: "i" } },
-          { sector: { $regex: "^" + req.body.search, $options: "i" } },
+          { name: { $regex: "^" + req.params.search, $options: "i" } },
+          { symbol: { $regex: "^" + req.params.search, $options: "i" } },
+          { sector: { $regex: "^" + req.params.search, $options: "i" } },
         ],
       },
       { page, limit: 5 },
     );
-
     res.json(result);
   } catch (e) {}
 });
+
 app.use("*", (req, res) => res.status(404).json({ error: "Route not found" }));
 
 export default app;
