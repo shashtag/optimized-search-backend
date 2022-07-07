27cf10e96fb7d80ac8a8be528c497d4219376c07
shahtag <shashwatsatna@gmail.com>
2022-07-08 00:33:55 +0530

    🩹 fixes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ dotenv.config();
 const port = process.env.PORT || 8000;
 
 mongoose
-  .connect(process.env.DB_URI)
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log(err));
 
